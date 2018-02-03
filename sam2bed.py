fe978ffa968e20338366d237752705c6f3bd8051
Alice Mayer <31933289+amayer21@users.noreply.github.com>
2018-02-03 16:51:36 +0000

    Update comments

--- sam2bed.py
+++ sam2bed.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 26 17:38:37 2018
-This is our SAM_to_BED.py @improved by Andreas
-
+Extract data from a SAM file and generate a BED file (using only basic Python commands)
+Added by Andreas:
 Define a function main that contains the code and is run at the end.
-Introduce log file properly. I think it's gone append that file each time
+Introduce log file properly. I think it's going to append that file each time
 we run the script => keep trace of all runs in 1 file.
 
 @author: group, edited by Andreas
