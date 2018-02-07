d3b2becc68c78796d67d6e567bc37d1b6fac0e1a
Alice Mayer <31933289+amayer21@users.noreply.github.com>
2018-02-07 17:56:42 +0000

    pipeline_crispr.py comment update
    Added comments

--- pipeline_crispr.py
+++ pipeline_crispr.py
@@ -2,10 +2,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb  7 14:00:34 2018
-
 @author: amayer
 
-docs removed
+Exercise: pipeline process fastq file from CRISPR screen to generate read count
+This is our first pipeline, so to keep it easy we use the trim function from Bowtie
+Therefore we can process only the file with a specific stagger (GFP-1)
+(To process all files, we'll need to use a regular expression to extract sgRNA instead)
+
+Library file had to be indexed before to start ($ bowtie-build FASTA_GeckoV2_HGLib_B.fasta geckob)
+
+- bowtie: trim 5' and 3' end and align it to GecKOv2A library
+- the result of bowtie is piped to samtools command to convert into BAM file
+- BAM file is sorted (may want to pipe that to previous command to avoid keeping intermediate files
+- sorted BAM file is indexed
+- statistics are extracted from BAM file into tsv file 
+
 """
 import sys
 import os
@@ -21,7 +32,9 @@ PARAMS = P.getParameters(
 # ---------------------------------------------------
 # Specific pipeline tasks
 
-
+# trim 5' and 3' end of reads to keep only the sgRNA sequence, 
+# then map reads to indexed library, then convert sam file into bam file
+# also generate a .error file for the alignement 
 @transform("*.fastq.gz",
            suffix(".fastq.gz"),
            ".bam")
@@ -32,7 +45,8 @@ def run_bowtie(infile, outfile):
 
     P.run()
 
-
+    
+# Sort the BAM file (could have been piped to previous command)
 @transform(run_bowtie,
            suffix(".bam"),
            "_sorted.bam")
@@ -43,6 +57,7 @@ def sort_bam(infile, outfile):
     P.run()
 
 
+# Index the sorted BAM file and create index file (.bam.bai)
 @transform(sort_bam,
            suffix(".bam"),
            ".bam.bai")
@@ -52,7 +67,8 @@ def index_bam(infile, outfile):
 
     P.run()
 
-
+# follows: wait for the index file to be created before to run next command
+# then compute statistics about the bam file and export them to tsv file
 @follows(index_bam)
 @transform(sort_bam,
            suffix(".bam"),
